ea44484dc2801276ee0a5347fa486f4d6c2675ba
Emma Hudgins <emma.hudgins@mail.mcgill.ca>
2021-11-20 21:18:12 -0500

    more debugging

--- biosecurity_model.R
+++ biosecurity_model.R
@@ -7,11 +7,8 @@ library(mgcv)
 
 dam<-read.csv('./data/damag.ex.or.csv')
 mg<-read.csv('./data/manag.ex.or.csv')
-sum<-read.csv('./data/national.sums.csv')
-sum$country<-gsub('Salvador', "El Salvador", sum$country)
 dam$code<-countrycode(dam$Official_country,'country.name', 'iso3c')
 mg$code<-countrycode(mg$Official_country,'country.name', 'iso3c')
-sum$code<-countrycode(sum$country,'country.name', 'iso3c')
 
 sr<-read_html('https://rainforests.mongabay.com/03highest_biodiversity.htm')
 sr_tab<-html_table(sr)[[1]]
@@ -27,24 +24,21 @@ sr_tab$tot_sr<-rowSums(sr_tab[,2:7], na.rm=T)
 
 dam$TenYear<-dam$Impact_year%/%10*10
 mg$TenYear<-mg$Impact_year%/%10*10
-n_refs_dam<-dam%>%group_by(TenYear, code)%>%summarize_at("Reference_ID", length)
-n_refs_mg<-mg%>%group_by(TenYear, code)%>%summarize_at("Reference_ID", length)
-dam_sum<-dam%>%group_by(Species, TenYear, code)%>%summarise_at("cost.bil", list(mean,length)) # not sure why a sum doesnt work in this line but i back out a sum from mean and length
-dam_sum$cost.bil<-dam_sum$fn1*dam_sum$fn2
+n_refs_dam<-dam%>%group_by(TenYear, code)%>%summarize_at("Reference_ID", n_distinct)
+n_refs_mg<-mg%>%group_by(TenYear, code)%>%summarize_at("Reference_ID", n_distinct)
+dam_sum<-dam%>%group_by(Species, TenYear, code)%>%summarise_at("cost.bil", sum) # not sure why a sum doesnt work in this line but i back out a sum from mean and length
 dam_sum<-subset(dam_sum, is.na(dam_sum$code)==F)
 
-mg_sum<-mg%>%group_by(Species, TenYear, code, Management_type)%>%summarise_at("cost.bil", list(mean,length)) # not sure why a sum doesnt work in this line but i back out a sum from mean and length
-mg_sum$cost.bil<-mg_sum$fn1*mg_sum$fn2
+mg_sum<-mg%>%group_by(Species, TenYear, code, Management_type)%>%summarise_at("cost.bil", sum) # not sure why a sum doesnt work in this line but i back out a sum from mean and length
 mg_sum<-subset(mg_sum, is.na(mg_sum$code)==F)
-mg_sum<-mg_sum[,c(1:4,7)]
 mg_sum2<-dcast(mg_sum, cost.bil+Species+code+TenYear~Management_type,mean, na.rm=T)
-combined<-merge(dam_sum, mg_sum2, by=c("Species", "code", "TenYear"))
+combined<-merge(dam_sum, mg_sum2, by=c("Species", "code", "TenYear"), all=T)
 
-combined<-combined[,c(1:3,7,8:11)]
+combined<-combined[,c(1:3,4,6:9)]
 colnames(combined)[c(4)]<-c( "damage_cost")
 
 ##add additional predictors
-combined<-merge(combined, sr_tab, 'code')
+combined<-merge(combined, sr_tab, 'code', all.x=T)
 socioeco_dat<-readRDS('./data/soc_econ_country.rds') # From Sardain, Leung et al. Nature Sustainability
 socioeco_dat<-subset(socioeco_dat, yr==2014)
 socioeco_dat<-subset(socioeco_dat, IHS.i%in%countrycode(unique(combined$code), 'iso3c', 'country.name'))
@@ -96,7 +90,7 @@ for (i in 1:nrow(combined))
 combined$mgmt_year[which(is.infinite(combined$mgmt_year))]<-2017
 
 ##matched data
-stwist_merge<-merge(combined, stwist_match, c('code', 'Species'))
+stwist_merge<-merge(combined, stwist_match, c('code', 'Species'), all.x=T)
 stwist_merge$mgmt_delay<-stwist_merge$TenYear.y-stwist_merge$mgmt_year # time since management began (can be negative)
 stwist_sum<-stwist_merge%>%group_by(code, TenYear.x)%>%summarize_at("mgmt_delay",mean, na.rm=T)                    
 
@@ -108,11 +102,11 @@ combined4$`Post-invasion management`[which(is.nan(combined4$`Post-invasion manag
 combined4$`Pre-invasion management`[which(is.nan(combined4$`Pre-invasion management`))]<-0
 combined4$`Knowledge funding`[which(is.nan(combined4$`Knowledge funding`))]<-0
 combined4$`Mixed`[which(is.nan(combined4$`Mixed`))]<-0
-combined4<-combined4%>%summarize_if(is.numeric, c(mean,length))
-combined4$damage_cost<-combined4$damage_cost_fn1*combined4$damage_cost_fn2
-combined4$Knowledge<-combined4$`Knowledge funding_fn1`*combined4$`Knowledge funding_fn2`
-combined4$pre_inv<-combined4$`Pre-invasion management_fn1`*combined4$`Pre-invasion management_fn2`
-combined4$post_inv<-combined4$`Post-invasion management_fn1`*combined4$`Post-invasion management_fn2`
+combined4<-combined4%>%summarize_if(is.numeric, c(mean,sum), na.rm=T)
+combined4$damage_cost<-combined4$damage_cost_fn2
+combined4$Knowledge<-combined4$`Knowledge funding_fn2`
+combined4$pre_inv<-combined4$`Pre-invasion management_fn2`
+combined4$post_inv<-combined4$`Post-invasion management_fn2`
 combined4$Knowledge[which(is.na(combined4$Knowledge)==T)]<-0
 combined4$pre_inv[which(is.na(combined4$pre_inv)==T)]<-0
 combined4$post_inv[which(is.na(combined4$post_inv)==T)]<-0
@@ -136,26 +130,27 @@ n_refs_dam$damage_ref<-n_refs_dam$Reference_ID
 n_refs_dam<-n_refs_dam[,-(3)]
 n_refs_mg$mg_ref<-n_refs_mg$Reference_ID
 n_refs_mg<-n_refs_mg[,-(3)]
-combined4<-merge(combined4, n_refs_dam)
-combined4<-merge(combined4, n_refs_mg)
+combined4<-merge(combined4, n_refs_dam, all.x=T)
+combined4<-merge(combined4, n_refs_mg, all.x=T)
+combined4$mg_ref[which(is.na(combined4$mg_ref))]<-0
 combined4$cum_species_fn1[which(is.na(combined4$cum_species_fn1))]<-0
 
-combined4<-combined4%>%group_by(code)%>%mutate(preinv_todate=cumsum(pre_inv), postinv_todate=cumsum(post_inv), knowledge_todate=cumsum(Knowledge))
-m<-gam(log(combined4$damage_cost_fn1+1)~log(combined4$Knowledge+1)+log(combined4$pre_inv+1)+log(combined4$post_inv+1)+log(combined4$cum_species_fn1+1)+log(combined4$tot_sr_fn1+1)+s(log(combined4$TenYear+1), k=5)+log(combined4$imports+1)+log(combined4$imports_historical+1)+(combined4$GDP.j+1)+(combined4$Pop.j+1)+combined4$mgmt_delay_fn1+log(combined4$damage_ref+1))
-sum_m<-summary(m)
+combined5<-combined4
+combined5$damage_ref[which(is.na(combined5$damage_ref))]<-0
+combined4<-combined4[which(combined4$damage_cost>0),]
+m<-gam(log(combined5$damage_cost+1)~log(combined5$Knowledge+1)+log(combined5$pre_inv+1)+log(combined5$post_inv+1)+log(combined5$cum_species_fn1+1)+log(combined5$tot_sr_fn1+1)+s(combined5$TenYear, k=5)+log(combined5$imports+1)+log(combined5$imports_historical+1)+(combined5$GDP.j+1)+(combined5$Pop.j+1)+combined5$mgmt_delay_fn1+log(combined5$damage_ref+1))
+(sum_m<-summary(m))
 write.csv(sum_m$p.table,file="damage_results.csv")
+write.csv(sum_m$s.table,file="damage_results_smooth.csv")
 
-# checking cumulative model
-m_1<-gam(log(combined4$damage_cost_fn1+1)~log(combined4$knowledge_todate+1)+log(combined4$preinv_todate+1)+log(combined4$postinv_todate+1)+log(combined4$cum_species_fn1+1)+log(combined4$tot_sr_fn1+1)+s(log(combined4$TenYear+1), k=5)+log(combined4$imports+1)+log(combined4$imports_historical+1)+(combined4$GDP.j+1)+(combined4$Pop.j+1)+combined4$mgmt_delay_fn1+log(combined4$damage_ref+1))
-summary(m_1) # fit not as strong
-
-
-combined4$mgmt_spend<-combined4$pre_inv+combined4$post_inv
+combined5$mgmt_spend<-combined5$pre_inv+combined5$post_inv
 
 
-m2<-gam(log(combined4$mgmt_spend+1)~log(combined4$pre_inv+1)+log(combined4$Knowledge+1)+log(combined4$damage_cost+1)+log(combined4$cum_species_fn1+1)+log(combined4$imports+1)+log(combined4$imports_historical+1)+(combined4$GDP.j+1)+log(combined4$Pop.j+1)+combined4$mgmt_delay_fn1+log(combined4$tot_sr_fn1+1)+log(combined4$mg_ref+1))
-sum_m2<-summary(m2)
+
+m2<-gam(log(combined5$mgmt_spend+1)~log(combined5$pre_inv+1)+log(combined5$Knowledge+1)+log(combined5$damage_cost+1)+log(combined5$cum_species_fn1+1)+log(combined5$imports+1)+log(combined5$imports_historical+1)+(combined5$GDP.j+1)+log(combined5$Pop.j+1)+combined5$mgmt_delay_fn1+log(combined5$tot_sr_fn1+1)+log(combined5$mg_ref+1)+s(combined5$TenYear, k=5))
+(sum_m2<-summary(m2))
 
 write.csv(sum_m2$p.table,file="mg_results.csv")
+write.csv(sum_m2$s.table,file="mg_results_smooth.csv")
 
     
